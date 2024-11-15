00516b783aed69266a2128565f1d50e1016eccbf
nchettir <nadir.chettir@edu.ece.fr>
2024-11-15 16:40:32 +0100

    ReducerHook

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,5 @@ app.get('/api/products', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
